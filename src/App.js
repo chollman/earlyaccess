b4dbb46dcda10848d960a40b9de78f7048426b7b
Florencia <flopitas2008@hotmail.com>
2022-11-09 22:20:20 -0300

    Pusimos el store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg'
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,7 +11,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          TODA
+          Tuki
         </a>
       </header>
     </div>
