ce38d7a60d6a338f1d71d20d4bd826ec84cd419d
Florencia <flopitas2008@hotmail.com>
2022-11-10 16:31:48 -0300

    Instalando sass, bootstrap y creamos navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import ArticlesList from './components/ArticlesList'
 
 const Home = (props) => {
-  const articles = useSelector((state) => state.articles.data)
+  const articles = useSelector((store) => store.articles.data)
   return (
     <div>
       <h3>Lista de Noticias</h3>
