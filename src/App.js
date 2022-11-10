5138d639698dbea246bd67f85cb6e7f6e89b2a94
Florencia <flopitas2008@hotmail.com>
2022-11-10 14:40:47 -0300

    Agregamos Contact y su route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import SharedLayout from './components/SharedLayout'
+import Contact from './pages/contact'
 import Home from './pages/home'
 
 function App() {
@@ -9,7 +9,9 @@ function App() {
       <Routes>
         <Route path='/' element={<SharedLayout />}>
           <Route index element={<Home />} />
+          <Route path='contacto' element={<Contact />} />
         </Route>
+
         <Route path='testing' element={<Home />} />
       </Routes>
     </BrowserRouter>
