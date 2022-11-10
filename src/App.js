f944fd25220ab919565182464041ff38bae0fb9b
Claudio Hollman <claudiohollman@gmail.com>
2022-11-10 14:43:14 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
           <Route index element={<Home />} />
           <Route path='contacto' element={<Contact />} />
         </Route>
-
         <Route path='testing' element={<Home />} />
       </Routes>
     </BrowserRouter>
