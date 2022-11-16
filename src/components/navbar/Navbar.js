d9fe14893a7c48b6202a38f5525de2557e6afdc0
Florencia <flopitas2008@hotmail.com>
2022-11-15 22:22:46 -0300

    Segui un toque con el navbar pero me quede sin tiempo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,34 @@
 import React from 'react'
 //import PropTypes from 'prop-types'
 import './navbar.scss'
-import { socialMedia } from './social-media'
+import { socialMedia } from '../../utils/social-media'
+import { Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
 
 const Navbar = (props) => {
   return (
     <nav className='navbar'>
       <h3> Tuki </h3>
       <ul>
-        <li>Noticias</li>
-        <li>Recomendado</li>
-        <li>Contacto</li>
+        <Link to='/' className='link'>
+          Últimas noticias
+        </Link>
+        <Link to='/contacto' className='link'>
+          asd
+        </Link>
+        <Link to='/contacto' className='link'>
+          Nosotros
+        </Link>
       </ul>
-      <ul >
+      <ul>
+        <div className='search-container'>
+          <BsSearch />
+          <input
+            className='search'
+            type='text'
+            placeholder='¿Que estas buscando?'
+          />
+        </div>
         {socialMedia.map(({ id, url, icon }) => {
           return (
             <li key={id}>
