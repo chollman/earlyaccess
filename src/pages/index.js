4398a8630fa12e220a012d83668a10e6310e5e0b
Claudio Hollman <claudiohollman@gmail.com>
2022-11-29 02:23:28 -0300

    refactor indexes en pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Home from './home'
-import Contact from './contact'
-import SingleArticle from './article'
+import Home from './home/Home'
+import Contact from './contact/Contact'
+import SingleArticle from './single-article/SingleArticle'
 import Error404 from './error/404'
 
 export { Home, Contact, SingleArticle, Error404 }
