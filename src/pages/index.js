756248cfcceffd9cdeaaf46477ec9d845ee797d7
Florencia <flopitas2008@hotmail.com>
2022-11-30 20:08:11 -0300

    Ya wey pofavo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from './home/Home'
 import Contact from './contact/Contact'
 import SingleArticle from './single-article/SingleArticle'
 import Error404 from './error/404'
+import ComingSoon from './coming-soon/ComingSoon'
 
-export { Home, Contact, SingleArticle, Error404 }
+export { Home, Contact, SingleArticle, Error404, ComingSoon }
